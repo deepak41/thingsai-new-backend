926c576340e1e93e1ee8e1213234cb5454456b35
deepak <deepak.singh@thethingscloud.com>
2019-06-25 08:40:23 +0530

    changes

--- app/routes/sessions.js
+++ app/routes/sessions.js
@@ -6,37 +6,64 @@ module.exports = function (router) {
     router.route('/')
        .post(function (req, res, next) {
 
-            User.findOne({email: req.body.email}).then(function (user) {
-                if (!user) {
-                    next({
-                        status: 401,
-                        message: messages.httpCodes(401)
-                    })
+            User.findOne({email: req.body.email}).then(function(user) {
 
-                } else {
-                    user.comparePassword(req.body.password, function (err, isMatch) {
-                        if(isMatch && !err) {
+                if (!user) return next({
+                    status: 401,
+                    message: "Invalid email or password!"
+                })
 
-                            var token = auth.signToken(user._id);
-                            res.json({
-                                error: false,
-                                token: 'Bearer ' + token,
-                                data: user
-                            });
-                        } else {
-                            next({
-                                status: 401,
-                                message: messages.httpCodes(401)
-                            })
-
-                        }
-
-                    });
-                }
-
-            }, function (err) {
+                user.comparePassword(req.body.password, function (err, isMatch) {
+                    if(isMatch && !err) {
+                        var token = auth.signToken(user._id);
+                        res.json({
+                            error: false,
+                            token: 'Bearer ' + token,
+                            data: user
+                        });
+                    } 
+                    else {
+                        next({
+                            status: 401,
+                            message: "Invalid email or password!"
+                        })
+                    }
+                });
+            }, 
+            function (err) {
                 next(err)
             })
-
         });
+
+
+    // This will handle the url calls for /api/sessions/firebaselogin
+    router.route('/firebaselogin')
+       .post(function (req, res, next) {
+
+            auth.verifyFireBaseToken(req.body.idToken, function(err, user) {
+
+                if(err) return next({
+                    status: 401,
+                    message: "Invalid email or password!"
+                });
+
+                User.findOne({email: user.email}).then(function(user) {
+
+                    if (!user) return next({
+                        status: 401,
+                        message: "Invalid email or password!"
+                    });
+
+                    var token = auth.signToken(user._id);
+
+                    res.json({
+                        error: false,
+                        token: 'Bearer ' + token,
+                        data: user
+                    });
+                })                
+
+            })
+        });
+
 }
--- config/environments/production.js
+++ config/environments/production.js
@@ -1,8 +1,10 @@
 var nconf = require('nconf');
 nconf.set('NODE_PORT', '80');
 nconf.set('url', 'http://localhost');
-nconf.set('secret', 'devdacticIsAwesome');
+nconf.set('secret', 'sunnyday');
 nconf.set('database', 'mongodb://localhost/thingsaidb');
 
-nconf.set('mserver', 'http://alerts.thethingscloud.com')
+nconf.set('mserver', 'http://newapi.thingsai.io.com')
 
+nconf.set('fserviceAccount', 'thingsai-test-firebase-adminsdk-54c39-0eec70d9db.json')
+nconf.set('fdatabaseURL', 'https://thingsai-test.firebaseio.com/');
