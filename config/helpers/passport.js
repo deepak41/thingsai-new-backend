18eb6a4999e6df91496e61993e914b0772803096
deepak <deepak.singh@thethingscloud.com>
2019-08-01 20:07:12 +0530

    changes

--- config/helpers/passport.js
+++ config/helpers/passport.js
@@ -5,8 +5,7 @@ module.exports = function(passport) {
 	var opts = {};
 	opts.jwtFromRequest = ExtractJwt.fromExtractors([ExtractJwt.fromAuthHeaderAsBearerToken()]);
 	opts.secretOrKey = nconf.get('secret');
-
-	passport.use(new JwtStrategy(opts, function(jwt_payload, done) {
-		done(jwt_payload);
+	passport.use(new JwtStrategy(opts, function(jwt_payload, callback) {
+		callback(jwt_payload);
 	}));
 };
